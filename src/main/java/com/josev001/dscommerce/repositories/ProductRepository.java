ed1f6145689e23d46f510db559d6a51486b7c8e9
JoseV-001 <Leaphaz@gmail.com>
2026-04-15 22:35:27 -0300

    feat: add product search by name with pagination

--- src/main/java/com/josev001/dscommerce/repositories/ProductRepository.java
+++ src/main/java/com/josev001/dscommerce/repositories/ProductRepository.java
@@ -1,10 +1,17 @@
 package com.josev001.dscommerce.repositories;
 
 import com.josev001.dscommerce.entities.Product;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.domain.Pageable;
+
+
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-
+    @Query("Select obj FROM Product obj " +
+            "WHERE UPPER(obj.name) LIKE UPPER(CONCAT('%',:name, '%'))")
+    Page<Product> searchByName(String name, Pageable pageable);
 
 }
